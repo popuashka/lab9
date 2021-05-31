4c2afdfaaacf8926e9bd4f55ac42afd59e67d288
popuashka <dubezhinskiy96@mail.ru>
2021-05-31 15:21:47 +0300

    Added entity

---
+++ src/main/java/bsu/rfe/Dubezhinskiy/entity/Identifiable.java
@@ -0,0 +1,5 @@
+package bsu.rfe.java.group8.lab9.Dubezhinskiy.varA4.entity;
+
+public interface Identifiable {
+    int getId();
+}
---
+++ src/main/java/bsu/rfe/Dubezhinskiy/entity/UserExistsException.java
@@ -0,0 +1,5 @@
+package bsu.rfe.java.group8.lab9.Dubezhinskiy.varA4.entity;
+
+public class UserExistsException extends Exception {
+    private static final long serialVersionUID = 584737643480913385L;
+}
